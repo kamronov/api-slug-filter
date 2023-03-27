2739e58c7e3f9aeb8635c9d1bdd9fcd341e6c684
kamronov <kamronovk3@gmail.com>
2023-03-27 11:37:06 +0500

    commit3

--- src/pages/Eror/Eror.jsx
+++ src/pages/Eror/Eror.jsx
@@ -4,8 +4,18 @@ import { Link } from 'react-router-dom'
 function Eror() {
   return (
     <div>
-        <h1>404 not fount</h1>
-        <Link to='/post/1'>Post/1</Link>
+      <h1>404 not fount</h1>
+      <li> <Link to='/post/1'>Post/1</Link></li>
+      <li><Link to='/post/2'>Post/2</Link></li>
+      <li><Link to='/post/3'>Post/3</Link></li>
+      <li><Link to='/post/4'>Post/4</Link></li>
+     <li> <Link to='/post/5'>Post/5</Link></li>
+     <li> <Link to='/post/6'>Post/6</Link></li>
+      <li><Link to='/post/7'>Post/7</Link></li>
+      <li><Link to='/post/8'>Post/8</Link></li>
+      <li><Link to='/post/9'>Post/9</Link></li>
+      <li><Link to='/post/10'>Post/10</Link></li>
+
     </div>
   )
 }
